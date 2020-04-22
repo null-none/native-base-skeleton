c009f05c39dfe25550170c3f6efa39a31e30be20
Dmitry Kalinin <dmitry.kalinin.email@gmail.com>
2020-04-22 23:44:44 +0300

    Updated npm and improved router

--- screens/navigation/MainStackNavigator.tsx
+++ screens/navigation/MainStackNavigator.tsx
@@ -7,19 +7,22 @@ import { createStore, applyMiddleware } from "redux";
 
 import reducer from "../reducer/store";
 import App from "../App";
+import { navigationRef, isMountedRef } from "./RootNavigation";
 
 const Stack = createStackNavigator();
 const store = createStore(reducer, applyMiddleware(thunk));
 
 function MainStackNavigator() {
+  React.useEffect(() => {
+    isMountedRef.current = true;
+    return () => (isMountedRef.current = false);
+  }, []);
+
   return (
     <Provider store={store}>
-      <NavigationContainer>
+      <NavigationContainer ref={navigationRef}>
         <Stack.Navigator>
-          <Stack.Screen
-            name="App"
-            component={App}
-          />
+          <Stack.Screen name="App" component={App} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
